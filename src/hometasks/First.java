17b634e80f0f53183def723faf208b362176c945
Lenovo <davidovic.karina0@gmail.com>
2024-01-05 20:25:30 +0100

    added the solution

--- src/hometasks/First.java
+++ src/hometasks/First.java
@@ -2,34 +2,30 @@ package hometasks;
 
 import java.util.Scanner;
 
-public class first {
+public class First {
     public static void main(String[] args) {
         //Напишите программу для учета средней оценки студента.
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number of subjects : ");
         int numberOfSub = scanner.nextInt();
-        int i, sum = 0;
-        for (i = 0; i < numberOfSub; i++) {
-            Scanner scannerNr = new Scanner(System.in);
+        int sum = 0;
+        for ( int i = 0; i < numberOfSub; i++) {
             System.out.println("Enter the mark : " + (i + 1));
-            int mark = scannerNr.nextInt();
+            int mark = scanner.nextInt();
             if (mark < 11) {
                 sum = mark + sum;
                 System.out.println(sum);
             }
             if (mark > 10) {
                 System.out.println("Start again. Please enter your marks correctly! From 1 to 10");
-                break;
+                return;
             }
         }
-        if (i != 0) {
-            int average = sum / (i);
-            System.out.println("Average is" + " " + average);
-        } else {
-            System.out.println("Average is unavailable");
+        double averageDouble = (double) sum / numberOfSub;
+        System.out.println("Average is" + " " + averageDouble);
 
         }
     }
-}
+
 
 
