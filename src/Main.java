e4639914419ca2b1f28aa6550086c9604b16c82a
Lenovo <davidovic.karina0@gmail.com>
2024-01-09 19:50:34 +0100

    solution of tasks lec-4

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,30 @@
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        // задание : найти сумму цифр ab + cd числа abcd
+        // Создайте программу, вычисляющую факториал натурального числа n, которое пользователь введёт с клавиатуры
+    /*int number = scanner();
+    int factorial = calculateFactorial(number);
+    System.out.println("Factorial is " + factorial);*/
+        // В американской армии считается несчастливым число 13, а в японской - 4.
+        // Перед международными учениями штаб армии решил исключить номера боевой техники, содержащие числа 4 или 13 (например, 40123, 13313, 12345 или 13040),
+        // чтобы не смущать иностранных коллег. Если в распоряжении армии имеется 100 тыс. единиц боевой техники и каждая боевая машина имеет номер от 00001 до 99999, то сколько всего номеров придётся исключить?
 
-        // отсканироват число
+    }
+
+    public static int scanner () {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the number : ");
-        int number = scanner.nextInt();
-        //перевести в строку
-        String numberStr = String.valueOf(number);
-        //отрезать первую половину строки, потом отрезать вторую часть строки
-        String fpart  = numberStr.substring(0, 2 );
-        String spart  = numberStr.substring(2);
-        // перевести их  обратно в числа int
-        int fhalfInt = Integer.parseInt(fpart);
-        int shalfInt = Integer.parseInt(spart);
-        // сложить
-        int x = fhalfInt + shalfInt;
-        // вывести
-        System.out.println("Sum is " + x );
+        return scanner.nextInt();
+    }
+
+    public static int calculateFactorial(int number) {
+        int factorial = 1;
+        for (int i = 1; i <= number; i++) {
+            factorial = i * factorial;
+        }
+        return factorial;
     }
 }
+
+
+
