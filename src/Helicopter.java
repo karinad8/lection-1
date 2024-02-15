4d0674889afc350f0ee98508125f3ad9de185d6f
Lenovo <davidovic.karina0@gmail.com>
2024-02-15 20:01:19 +0100

    solution

---
+++ src/Helicopter.java
@@ -0,0 +1,6 @@
+public class Helicopter extends AirlineOne{
+
+    public Helicopter(double capacity, double loadCapacity, String color, String name, double releaseYear, double flightRange) {
+        super(capacity, loadCapacity, color, name, releaseYear, flightRange);
+    }
+}
