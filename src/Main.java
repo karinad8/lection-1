b5609b97b5a858d7cfd4f0d451d162f456f3c827
Lenovo <davidovic.karina0@gmail.com>
2024-03-17 19:10:35 +0100

    solution

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+public class Main {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        List<Integer> listOfNumbers = Arrays.asList(5, 56, 8, 91, 7);
+        squareAndPrintTheNumbers(listOfNumbers);
+    }
+
+    public static void squareAndPrintTheNumbers(List<Integer> numbers) throws ExecutionException, InterruptedException {
+        List<Integer> NumbersSquared = new ArrayList<>();
+        List<CompletableFuture<Integer>> completableFutureList = new ArrayList<>();
+
+        for (int el : numbers) {
+            CompletableFuture<Integer> numbersCompletableFuture = CompletableFuture.supplyAsync(() -> (int) Math.pow(el, 2));
+            completableFutureList.add(numbersCompletableFuture);
+
+        }
+        for (CompletableFuture<Integer> future : completableFutureList) {
+            NumbersSquared.add(future.get());
+
+        }
+        System.out.println("Numbers squared : " + NumbersSquared);
+    }
+}
