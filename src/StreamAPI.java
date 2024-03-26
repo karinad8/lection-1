0afb143b010e3ddd1a4f9b3b1323c251e5da269f
Lenovo <davidovic.karina0@gmail.com>
2024-03-26 10:36:54 +0100

    solution

--- src/StreamAPI.java
+++ src/StreamAPI.java
@@ -2,23 +2,26 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StreamAPI {
     public static void main(String[] args) {
 
         List<String> myList = Arrays.asList("a1", "a2", "a3", "b1", "b3", "c2", "c1", "c5");
         System.out.print("Сортировка по возрастанию цифр: \n");
-        myList.stream()
+        List<String> sortedList = myList.stream()
                 .sorted(Comparator.comparingInt(s -> Integer.valueOf(s.substring(1))))
-                .forEach(System.out::println);
+                .toList();
+        System.out.println(sortedList);
 
         Collections.sort(myList, Collections.reverseOrder());
         System.out.println("Сортировка по убыванию букв: " + myList);
 
-        List<String> filteredList = myList.stream()
+        List<String> filteredList = sortedList.stream()
                 .filter(object -> !object.matches("[abc]3"))
                 .skip(1)
-                .limit(4)
+                .limit(sortedList.size())
                 .map(String::toUpperCase)
                 .toList();
         System.out.println(filteredList);
